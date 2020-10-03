7ebf4f36818e17cba0225ca8f16521c7bcc92048
MaGnsi0 <magnsi0o0@gmail.com>
2020-10-03 13:52:33 +0200

    Update Codeforces

---
+++ Codeforces/Codeforces Round #459 (Div. 2)/B. Radio Station.cpp
@@ -0,0 +1,29 @@
+//https://codeforces.com/problemset/problem/918/B
+#include <bits/stdc++.h>
+using namespace std;
+#define F first 
+#define S second
+typedef long long ll;
+typedef long double ld;
+ll mod = 1e9 + 7;
+
+int main ()
+{
+	ios_base::sync_with_stdio (0); cin.tie (0); cout.tie (0);
+	int n, m;
+	cin >> n >> m;
+	vector<string> v1(n), v2(n), v3(m), v4(m);
+	for (int i = 0; i < n; ++i) cin >> v1[i] >> v2[i];
+	for (int i = 0; i < m; ++i) cin >> v3[i] >> v4[i];
+	for (int i = 0; i < m; ++i)
+	{
+		cout << v3[i] << " " << v4[i] << " #";
+		for (int j = 0; j < n; ++j)
+		{
+			if (v4[i] == v2[j] + ';')
+			{
+				cout << v1[j] << "\n";
+			}
+		}
+	}
+}
