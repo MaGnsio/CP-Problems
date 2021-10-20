35b70da07ec19bb6948fd70a6eda19deac732d0f
Muhammed Gehad <magnsi0o0@gmail.com>
2021-10-20 21:12:30 +0200

    Update All (ROG)

--- AIC/AIC'20 -Level 2 Training/Week #3.1/V - Just Solve It! (4).py
+++
@@ -1,9 +0,0 @@
-#https://vjudge.net/contest/419722#problem/V
-def gcd (x, y):
- if y == 0:
-  return x
- return gcd (y, x % y)
-x, y, a, b = input ().split ()
-n, m, l, r = int (x), int (y), int (a), int (b)
-f = int (n * m / gcd (n, m))
-print (int ((r // f) - ((l - 1) // f)))
--- Codeforces/Rounds/Codeforces Beta Round #85 (Div. 2 Only)/A. Petya and Strings.cpp
+++
@@ -1,9 +0,0 @@
-//https://codeforces.com/problemset/problem/112/A
-#include <bits/stdc++.h>
-
-int main ()
-{
-	char f[101], s[101];
-	std::cin >> s >> f;
-	std::cout << stricmp (s,f);
-}
