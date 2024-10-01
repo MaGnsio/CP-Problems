9e4fc4f05a084e9a7f8f56465d48f2c3a54fcf2d
MaGnsio <magnsi0o0@gmail.com>
2024-10-01 20:54:13 +0300

    practice

--- Codeforces/Kotlin Heros/Kotlin Heroes: Episode 11/A.kt
+++ Codeforces/Kotlin Heros/Kotlin Heroes: Episode 11/A.kt
@@ -1,7 +1,7 @@
 fun main() {
-    repeat(readInt()) {
-        val n = readln.toInt()
-        val a = readStrings().map { it.toInt() }
+    repeat(readln().toInt()) {
+        val n = readln().toInt()
+        val a = readln().split(" ").map { it.toInt() }
         val x = a.dropLast(1).max()
         if (a[n - 1] == x + 1) {
             println(x)
